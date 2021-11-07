5c20beb11651e70b866b1895e448021bf17a6fc1
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-07 13:10:57 +0530

    Update youtube_dl_button.py

--- anydlbot/plugins/youtube_dl_button.py
+++ anydlbot/plugins/youtube_dl_button.py
@@ -13,7 +13,7 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 # https://stackoverflow.com/a/37631799/4723940
 from PIL import Image
-from pyrogram import InputMediaPhoto
+
 from anydlbot import (
     DOWNLOAD_LOCATION,
     TG_MAX_FILE_SIZE,
